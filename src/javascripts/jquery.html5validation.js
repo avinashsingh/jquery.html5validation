7fcacac5e20347b97fc0424cde04c2a4a4d07069
Avinash Singh <avinash.singh@mettl.com>
2014-03-02 12:50:50 +0530

    - Added file

---
+++ src/javascripts/jquery.html5validation.js
@@ -0,0 +1,103 @@
+/**
+ * A fallback for HTML5 elements attributes.
+ * Currently handling only input tag inside any form.
+ * Hanlded attributes:
+ * type => number, text
+ *  -> min, max, step
+ * required
+ *
+ * @author: Avinash Singh
+ */
+
+$( function () {
+  // which all elements to iterate on
+  var validatableDomElements = ["input"],
+    validations = undefined;
+
+  // create error on UI
+  function showError( element, errorText ) {
+    var html = $( "<div style='position: absolute;'>" + errorText + "</div>"),
+        $el = $( element );
+
+    if ( $el.data("showingWarning") ) {
+      return;
+    }
+
+    function removeWarning () {
+      $el.unbind( "focus", removeWarning );
+      $el.removeData("showingWarning");
+      html.remove();
+    }
+    
+    $el.bind( "focus", removeWarning );
+    setTimeout( removeWarning, 4000 );
+
+    $("body").add( html );
+
+    html.css( "top" , $el.offset().top + html.height() ).
+        css( "left", $el.offset().left );
+
+    $("body").animate({scrollTop: $el.offset().top - 100});
+  }
+
+  function Validations() {
+
+    function validateInput () {
+      var $this = $(this);
+      if ( $this.attr("required") ) {
+        if ( $.trim( $this.val() ).length == 0 ) {
+          showError( this, "This is a required field" );
+          return false;
+        }
+      }
+
+      if ( $this.attr("type") === "number" ) {
+        var numberValue;
+
+        if ( !$.isNumeric( $this.val() ) ) {
+          showError( this, "This should must contain numbers only" );
+          return false;
+        }
+
+        numberValue = parseFloat( $this.val(), 5 );
+
+        if ( $this.attr("step") !== undefined && ( numberValue % $this.sttr("step") !== 0 ) ) {
+          showError( this, "The value should step by " + $this.attr("step") );
+          return false;
+        } else if ( $this.attr("min") !== undefined && ( numberValue < $this.attr("min") ) ) {
+          showError( this, "Minimum allowed value is " + $this.attr("min") );
+          return false;
+        } else if ( $this.attr("max") !== undefined && ( numberValue > $this.attr("max") ) ) {
+          showError( this, "Maximum allowed value is " + $this.attr("max") );
+          return false;
+        }
+      }
+    }
+
+    function validate () {
+      if( $(this).is("input") ) {
+        return validateInput.call( this );
+      }
+    }
+
+    $(document).on ( "submit", "form", function (e) {
+      var continued = false;
+      $.each( $(this).find("input"), function ( index, input) {
+        if ( !continued ) return;
+        if( !validate.call(input) ) {
+          continued = false;
+          e.preventDefault();
+        }
+      });
+    });
+    return {};
+  };
+
+  $.html5validation = function () {
+    if ( validations !== undefined ) {
+      validations = new Validations();
+    }
+    return validations;
+  };
+
+});
